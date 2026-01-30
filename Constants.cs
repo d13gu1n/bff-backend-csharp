aa93bf27e72cff5792d74b2dd2e9517026f768e1
dacevedom <d.acevedom1993@gmail.com>
2026-01-30 11:27:44 -0300

    Deleted extra files from previous .NET 10 implementation

--- Constants.cs
+++
@@ -1,7 +0,0 @@
-namespace Api;
-
-public static class Constants
-{
-    public const string SECRET_KEY = "This is my custom and very very very extremely long Secret key...";
-    public const string ISSUER = "http://localhost:5000";
-}
