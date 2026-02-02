492c5053e09648c75486c42e2c925817d0425c31
dacevedom <d.acevedom1993@gmail.com>
2026-02-02 13:04:19 -0300

    Created Angular Frontend part of BFF app

---
+++ src/Angular/ClientApp/src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthernticationService } from "../shared/authentication.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  public isBusy = false;
+  public loginModel: {
+    username?: string,
+    password?: string
+  } = {};
+
+  constructor(
+    private authService: AuthenticationService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.isBusy) {
+      return;
+    }
+
+    this.isBusy = true;
+
+    this.authService.login(this.loginModel.username!, this.loginModel.password!)
+      .subscribe(_ => {
+        window.location.href = "/home";
+      })
+      .add(() => this.isBusy = false);
+  }
+}
--- src/Angular/Program.cs
+++ src/Angular/Program.cs
@@ -1,27 +1,28 @@
+
+using Angular;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
+builder.Services.AddBffProxy(builder.Configuration);
+builder.Services.AddLocalAuthentication();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseRouting();
 
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
+app.UseAuthentication();
+app.useAuthorization();
 
+app.MapReverseProxy();
+app.MapEndpoints(builder.Configuration);
 app.MapFallbackToFile("index.html");
 
 app.Run();
