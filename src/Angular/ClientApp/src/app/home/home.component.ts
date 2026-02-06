eee326d8140a8ae97067ded940c431116b984b71
dacevedom <d.acevedom1993@gmail.com>
2026-02-06 11:48:40 -0300

    Deleted extra navbar from Home component, fixed Proxy endpoint addresses

--- src/Angular/ClientApp/src/app/home/home.component.ts
+++ src/Angular/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../shared/authentication.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  public currentUser: any;
+
+  constructor(
+    private authService: AuthenticationService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.currentUser = this.authService.getUser();
+  }
 }
